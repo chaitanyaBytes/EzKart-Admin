3f922e8f1fdfbe323c741a99378dd27c24366bf1
chaitanyaBytes <chaitanyagupta172004@gmail.com>
2024-08-22 17:47:54 +0530

    add category routes

---
+++ ezkart-admin/app/api/[storeId]/categories/route.ts
@@ -0,0 +1,75 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prismadb";
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { name, billboardId } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!billboardId) {
+      return new NextResponse("Billboard Id is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store Id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prisma.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorised", { status: 403 });
+    }
+
+    const category = await prisma.category.create({
+      data: {
+        name,
+        billboardId,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("[CATEGORIES_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("StoreId is required", { status: 400 });
+    }
+
+    const categories = await prisma.category.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(categories);
+  } catch (error) {
+    console.log("[CATEGORIES_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
